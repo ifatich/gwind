7022cdbd827239921eea90f0e4acd65f65183aa4
Lord <ifatich@gmail.com>
2026-08-18 14:48:31 +0700

    style(popover): adjust container radius and header padding

--- packages/ui/src/components/ui/popover/PopoverContent.vue
+++ packages/ui/src/components/ui/popover/PopoverContent.vue
@@ -29,12 +29,12 @@ const portalTarget = useGwindPortalTarget()
       v-bind="{ ...forwarded, ...$attrs }"
       :class="
         cn(
-          'z-50 w-72 overflow-hidden rounded-2xl border border-black-100 bg-white text-black-800 shadow-drop-1 outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+          'z-50 w-72 overflow-hidden rounded-md border border-black-100 bg-white text-black-800 shadow-drop-1 outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           props.class,
         )
       "
     >
-      <div v-if="props.title || $slots.header" class="flex flex-col gap-y-1 p-4">
+      <div v-if="props.title || $slots.header" class="flex flex-col gap-y-1 p-4 pb-1">
         <slot name="header">
           <h3 class="text-omicron font-bold leading-none tracking-tight text-black-800">
             {{ props.title }}
