8266eec78fdf8c552fe85596aa18ec67c363cd10
Lord <ifatich@gmail.com>
2026-08-18 14:37:18 +0700

    style(tooltip): align header and content padding

--- packages/ui/src/components/ui/tooltip/TooltipContent.vue
+++ packages/ui/src/components/ui/tooltip/TooltipContent.vue
@@ -29,12 +29,12 @@ const portalTarget = useGwindPortalTarget()
       v-bind="{ ...forwarded, ...$attrs }"
       :class="
         cn(
-          'z-50 overflow-hidden rounded-2xl border border-black-100 bg-white text-black-800 shadow-drop-1 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+          'z-50 overflow-hidden rounded-md border border-black-100 bg-white text-black-800 shadow-drop-1 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           props.class,
         )
       "
     >
-      <div v-if="props.title || $slots.header" class="flex flex-col gap-y-1 p-3">
+      <div v-if="props.title || $slots.header" class="flex flex-col gap-y-1 px-3 py-2 pb-1">
         <slot name="header">
           <h3 class="text-omicron font-bold leading-none tracking-tight text-black-800">
             {{ props.title }}
